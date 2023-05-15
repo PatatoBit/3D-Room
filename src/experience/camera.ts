ca68e548cc629647435156c55ba3b6b7338e859f
Patato <pathonbusiness@gmail.com>
2023-05-15 21:55:23 +0700

    update camera position

--- src/experience/camera.ts
+++ src/experience/camera.ts
@@ -29,7 +29,7 @@ export default class Camera {
       0.1,
       100
     );
-    this.instance.position.set(6, 4, 8);
+    this.instance.position.set(5, 3, 5);
     this.scene.add(this.instance);
   }
 
